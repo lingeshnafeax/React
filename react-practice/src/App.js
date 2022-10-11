6fb14602d222a21df39b5d92ffcfaaf17e6b80ce
lingesh <wlingesh260@gmail.com>
2022-10-11 19:23:37 +0530

    day 1 before dinner

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,13 +1,57 @@
-import ExpenseItem from './components/ExpenseItem';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
+import "./App.css";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 790,
+      date: new Date(2022, 1, 23),
+    },
+    {
+      id: "e2",
+      title: "Movies",
+      amount: 500,
+      date: new Date(2022, 4, 28),
+    },
+    {
+      id: "e3",
+      title: "WiFi",
+      amount: 750,
+      date: new Date(2022, 1, 12),
+    },
+    {
+      id: "e4",
+      title: "Pocket Money",
+      amount: 1000,
+      date: new Date(2022, 3, 16),
+    },
+  ];
   return (
     <div className="App">
       <h1>Let's get started!</h1>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
     </div>
-
   );
 }
 
--- react-practice/src/components/ExpenseItem.js
+++ react-practice/src/components/ExpenseItem.js
@@ -1,11 +1,18 @@
 import "./ExpenseItem.css";
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const date = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>January 07th 2004</div>
+      <div>
+        <div>{date}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$89.76</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
